5088510a4865f4aa271b0023bd5801229bad8c26
Jon goldberg <jon@palantetech.coop>
2017-12-24 15:55:36 -0500

    Flesh out gethosts.php - not done yet

--- gethosts.php
+++ gethosts.php
@@ -1,13 +1,14 @@
 #!/usr/bin/php
 <?php
 $test = [
-         "megaphone_saas" => [
+/*         "megaphone_saas" => [
            "hosts" => [
              "server1.megaphonetech.com",
              "server2.megaphonetech.com",
              "server3.megaphonetech.com",
             ],
          ],
+*/
          "lamp" => [
            "hosts" => [
              "orange.megaphonetech.com",
@@ -33,4 +34,32 @@ $test = [
            ]
          ]
         ];
-echo json_encode($test);
+function help() {
+  echo "Usage:\n";
+  echo " --help               This message\n";
+  echo " --list               Show all hosts\n";
+  echo " --host <hostname>    Show a single host specified by <hostname>";
+}
+
+function run($argv) {
+  if (empty($argv[1])) {
+    echo "No command found.\n\n";
+    help();
+    exit;
+  }
+  switch ($argv[1]) {
+    case '--list':
+      echo json_encode($test);
+      break;
+
+    case '--host':
+      echo json_encode($test);
+      break;
+
+    default:
+      echo "Unknown command.\n\n";
+    case '--help':
+      help();
+  }
+}
+run($argv);
