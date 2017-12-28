44fa57946c2a5b45f5935666829a02fb21dd4a2f
Jon goldberg <jon@palantetech.coop>
2017-12-28 18:54:10 -0500

    New gethosts - uses Services instead of Views JSON

--- gethosts.php
+++ gethosts.php
@@ -1,65 +1,62 @@
-#!/usr/bin/php
 <?php
-$test = [
-/*         "megaphone_saas" => [
-           "hosts" => [
-             "server1.megaphonetech.com",
-             "server2.megaphonetech.com",
-             "server3.megaphonetech.com",
-            ],
-         ],
-*/
-         "lamp" => [
-           "hosts" => [
-             "orange.megaphonetech.com",
-             "nwu.org",
-           ],
-           "children" => [
-             "megaphone_saas",
-           ],
-         ],
-         "docker" => [
-           "hosts" => [
-             "red.megaphonetech.com",
-           ],
-         ],
-         "_meta" => [
-           "hostvars" => [
-             "orange.megaphonetech.com" => [
-               "ansible_user" => "root",
-             ],
-             "nwu.org" => [
-               "ansible_user" => "root",
-             ]
-           ]
-         ]
-        ];
-function help() {
-  echo "Usage:\n";
-  echo " --help               This message\n";
-  echo " --list               Show all hosts\n";
-  echo " --host <hostname>    Show a single host specified by <hostname>";
+
+const ENDPOINT = 'https://crm.megaphonetech.com/rest/';
+const USERNAME = 'restuser';
+const PASSWORD = 'dbSfzFqvHXDHBhCy1wI4OO1dKhH2ResX';
+
+/**
+ *  Get the CSRF header.
+ *  $curl_options array the curl options for this transaction.
+ */
+
+function post_csrf_header() {
+  $csrfToken = post([], 'user/token.json', NULL);
+  return 'X-CSRF-Token: ' . $csrfToken->token;
 }
 
-function run($argv) {
-  if (empty($argv[1])) {
-    echo "No command found.\n\n";
-    help();
-    exit;
+function post($curlHeaders, $operation, $postFields = NULL) {
+  $curlHeaders[] = 'Content-Type: application/json';
+  $curlOptions = [
+    CURLOPT_HTTPHEADER => $curlHeaders,
+    CURLOPT_RETURNTRANSFER => TRUE,
+    CURLOPT_POST => 1,
+    CURLOPT_URL => ENDPOINT . $operation,
+    CURLOPT_POSTFIELDS => $postFields ? json_encode($postFields) : NULL,
+  ];
+  $curl = curl_init();
+  curl_setopt_array($curl, $curlOptions);
+  $result = curl_exec($curl);
+  // DEBUG shit
+  if ($operation == 'node') {
+    print_r($curlOptions[CURLOPT_HTTPHEADER]);
+    print_r(curl_getinfo($curl));
   }
-  switch ($argv[1]) {
-    case '--list':
-      echo json_encode($test);
-      break;
-
-    case '--host':
-      echo json_encode($test);
-      break;
-
-    default:
-      echo "Unknown command.\n\n";
-    case '--help':
-      help();
-  }
+  curl_close($curl);
+  return json_decode($result);
 }
-run($argv);
+
+function get($curlHeaders, $operation, $body = NULL) {
+  $curlOptions = [
+    CURLOPT_HTTPHEADER => $curlHeaders,
+    CURLOPT_RETURNTRANSFER => TRUE,
+    CURLOPT_URL => ENDPOINT . $operation,
+  ];
+  $curl = curl_init();
+  curl_setopt_array($curl, $curlOptions);
+  $result = curl_exec($curl);
+  curl_close($curl);
+  return json_decode($result);
+}
+
+// This all is the CSRF token + login process
+$headers[] = post_csrf_header();
+$creds = [
+  'username' => USERNAME,
+  'password' => PASSWORD,
+];
+
+$result = post($headers, 'user/login.json', $creds);
+$headers[] = "Cookie: $result->session_name=$result->sessid";
+
+$result =  get($headers, 'node?parameters[type]=server', NULL);
+print_r($result);
