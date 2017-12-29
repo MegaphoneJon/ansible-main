f69b5549c3a9b36041fa312adf118d00383e5655
Jon goldberg <jon@palantetech.coop>
2017-12-28 20:46:21 -0500

    A bit of rearranging and adding comments

--- gethosts.php
+++ gethosts.php
@@ -5,9 +5,49 @@ const ENDPOINT = 'https://crm.megaphonetech.com/rest/';
 const USERNAME = 'restuser';
 const PASSWORD = 'dbSfzFqvHXDHBhCy1wI4OO1dKhH2ResX';
 
+
+
+// This all is the CSRF token + login process
+$headers[] = post_csrf_header();
+$creds = [
+  'username' => USERNAME,
+  'password' => PASSWORD,
+];
+
+$result = post($headers, 'user/login.json', $creds);
+$headers[] = "Cookie: $result->session_name=$result->sessid";
+
+// Pull the view that shows the list of Ansible-enabled servers from Drupal.
+$result =  get($headers, 'views/server_list?display_id=services_1', NULL);
+$inventory = buildServerList($result);
+echo $inventory;
+
+
+
+
+/*
+ * Accepts an array from Drupal Services Views, outputs an Ansible-compatible host list.
+ *
+ * @param array $view
+ *   The array that holds all the db ids.
+ *
+ * @return string $inventory This is a JSON-encoded inventory file in the format Ansible expects.
+ */
+
+function buildServerList($view) {
+  $inventory['hosts'] = [];
+  foreach ($view as $server) {
+    $inventory['hosts'][] = $server->fqdn;
+    if ($server->username) {
+      $inventory['_meta']['hostvars'][$server->fqdn]['ansible_user'] = $server->username;
+    }
+  }
+  return json_encode($inventory);
+}
+
 /**
  *  Get the CSRF header.
- *  $curl_options array the curl options for this transaction.
+ *  @return array the curl options for this transaction.
  */
 
 function post_csrf_header() {
@@ -47,39 +87,4 @@ function get($curlHeaders, $operation, $body = NULL) {
   $result = curl_exec($curl);
   curl_close($curl);
   return json_decode($result);
-}
-
-// This all is the CSRF token + login process
-$headers[] = post_csrf_header();
-$creds = [
-  'username' => USERNAME,
-  'password' => PASSWORD,
-];
-
-$result = post($headers, 'user/login.json', $creds);
-$headers[] = "Cookie: $result->session_name=$result->sessid";
-
-$result =  get($headers, 'views/server_list?display_id=services_1', NULL);
-//print_r($result);
-
-$inventory = buildServerList($result);
-echo json_encode($inventory);
-/*
- * Accepts an array from Drupal Services Views, outputs an Ansible-compatible host list.
- *
- * @param array $view
- *   The array that holds all the db ids.
- *
- * @return CRM_Contribute_BAO_Contribution|\CRM_Core_Error
- */
-
-function buildServerList($view) {
-  $inventory['hosts'] = [];
-  foreach ($view as $server) {
-    $inventory['hosts'][] = $server->fqdn;
-    if ($server->username) {
-      $inventory['_meta']['hostvars'][$server->fqdn]['ansible_user'] = $server->username;
-    }
-  }
-  return $inventory;
 }
