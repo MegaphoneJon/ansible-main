82f2f95fc5ecf450169d23bb3d5e61954cdba2bb
Jon goldberg <jon@palantetech.coop>
2017-12-29 11:27:47 -0500

    Support loading a single host or --help

--- gethosts.php
+++ gethosts.php
@@ -5,27 +5,39 @@ const ENDPOINT = 'https://crm.megaphonetech.com/rest/';
 const USERNAME = 'restuser';
 const PASSWORD = 'dbSfzFqvHXDHBhCy1wI4OO1dKhH2ResX';
 
+function run($argv) {
+  if (empty($argv[1])) {
+    echo "No command found.\n\n";
+    help();
+    exit;
+  }
+  switch ($argv[1]) {
+    case '--list':
+      $headers = login();
+      // Pull the view that shows the list of Ansible-enabled servers from Drupal.
+      $result = get($headers, 'views/server_list?display_id=services_1', NULL);
+      $inventory = buildServerList($result);
+      echo $inventory;
+      break;
+
+    case '--host':
+      $headers = login();
+      // Pull the view that shows the list of Ansible-enabled servers from Drupal.
+      $result = get($headers, "views/server_list?display_id=services_1&title=$argv[2]", NULL);
+      $inventory = buildServerList($result);
+      echo $inventory;
+      break;
+
+    default:
+      echo "Unknown command.\n\n";
+    case '--help':
+      help();
+  }
+}
+run($argv);
 
 
-// This all is the CSRF token + login process
-$headers[] = post_csrf_header();
-$creds = [
-  'username' => USERNAME,
-  'password' => PASSWORD,
-];
-
-$result = post($headers, 'user/login.json', $creds);
-$headers[] = "Cookie: $result->session_name=$result->sessid";
-
-// Pull the view that shows the list of Ansible-enabled servers from Drupal.
-$result =  get($headers, 'views/server_list?display_id=services_1', NULL);
-$inventory = buildServerList($result);
-echo $inventory;
-
-
-
-
-/*
+/**
  * Accepts an array from Drupal Services Views, outputs an Ansible-compatible host list.
  *
  * @param array $view
@@ -33,7 +45,6 @@ echo $inventory;
  *
  * @return string $inventory This is a JSON-encoded inventory file in the format Ansible expects.
  */
-
 function buildServerList($view) {
   $inventory['hosts'] = [];
   foreach ($view as $server) {
@@ -47,10 +58,9 @@ function buildServerList($view) {
 }
 
 /**
- *  Get the CSRF header.
- *  @return array the curl options for this transaction.
+ * Get the CSRF header.
+ * @return array the curl options for this transaction.
  */
-
 function post_csrf_header() {
   $csrfToken = post([], 'user/token.json', NULL);
   return 'X-CSRF-Token: ' . $csrfToken->token;
@@ -83,4 +93,28 @@ function get($curlHeaders, $operation, $body = NULL) {
   $result = curl_exec($curl);
   curl_close($curl);
   return json_decode($result);
+}
+
+/**
+ * Handle login (including getting a CSRF header)
+ * @return array $headers The necessary headers to perform further POST/GET actions.
+ */
+function login() {
+  // This all is the CSRF token + login process
+  $headers[] = post_csrf_header();
+  $creds = [
+    'username' => USERNAME,
+    'password' => PASSWORD,
+  ];
+
+  $result = post($headers, 'user/login.json', $creds);
+  $headers[] = "Cookie: $result->session_name=$result->sessid";
+  return $headers;
+}
+
+function help() {
+  echo "Usage:\n";
+  echo " --help               This message\n";
+  echo " --list               Show all hosts\n";
+  echo " --host <hostname>    Show a single host specified by <hostname>";
 }
