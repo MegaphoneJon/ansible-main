e790555467e434c075d75ec9136c26837318359a
Jon Goldberg <jon@megaphonetech.com>
2018-04-13 16:50:42 -0400

    Finish the upgrade-civi helper script; successfully manipulate its output

--- roles/upgrade-civi/helpers/upgradeable-civi.php
+++ roles/upgrade-civi/helpers/upgradeable-civi.php
@@ -1,9 +1,17 @@
+#!/usr/bin/php
 <?php
-$e = new CRM_Admin_Page_Extensions; 
-$localExtensionList = $e->formatLocalExtensionRows(); 
-$remoteExtensionList = $e->formatRemoteExtensionRows($localExtensionList); 
-foreach($remoteExtensionList as $extension) { 
-  if ($extension['is_upgradeable']) { 
-    echo $extension['id'] . "\n";
+eval(`cv php:boot`);
+
+$e = new CRM_Admin_Page_Extensions();
+$localExtensionList = $e->formatLocalExtensionRows();
+$remoteExtensionList = $e->formatRemoteExtensionRows($localExtensionList);
+$i = 0;
+foreach ($remoteExtensionList as $e) {
+  if ($e['is_upgradeable']) {
+    $upgradeableExtensions[$i]['id'] = $e['id'];
+    $upgradeableExtensions[$i]['name'] = $e['name'];
+    $upgradeableExtensions[$i]['version'] = $e['version'];
+    $i++;
   }
 }
+print_r(json_encode($upgradeableExtensions));
